40b73983b3eac0e30082b33401d9fa4a25b75ea7
H/S <kidoairakusyunkasyuutou@gmail.com>
2019-03-06 18:47:20 +0900

    add sample

--- SeleniumSample/Program.cs
+++ SeleniumSample/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,22 @@ namespace SeleniumSample
     {
         static void Main(string[] args)
         {
+            var service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+
+            var options = new ChromeOptions();
+            options.AddArguments("--headless");
+
+            using (IWebDriver driver = new ChromeDriver(service, options))
+            {
+                driver.Url = @"https://www.google.com/";
+
+                Console.Write(driver.PageSource);
+                
+                driver.Quit();
+            }
+
+            Console.ReadKey();
         }
     }
 }
